6c2a7df8f3a4a847c3df16d91033f1f6e3f7b447
PixelFritz <twi.wonder@gmail.com>
2024-09-23 00:16:34 -0300

    Atualização do html, css e particulas

---
+++ particles-js.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+    particlesJS('particles-js', {
+        particles: {
+            number: { value: 300 },
+            color: { 
+                value: ['#b6e1ec', '#f5b0ec', '#f1f095', '#e4e4dd'],
+                random: true
+            },
+            shape: {
+                type: 'circle'
+            },
+            size: {
+                value: 6,
+                random: true,
+                anim: {
+                    speed: 6,
+                    size_min: 2
+                }
+            },
+            opacity: {
+                value: 1,
+                random: true,
+                anim: {
+                    enable: true,
+                    speed: 1,
+                    opacity_min: 0.5
+                }
+            },
+            move: {
+                enable: true,
+                speed: 3,
+                direction: 'none',
+                random: false,
+                straight: false,
+                out_mode: 'out'
+            },
+            line_linked: {
+                enable: false
+            }
+        },
+        interactivity: {
+            events: {
+                onhover: {
+                    enable: false
+                }
+            }
+        }
+    });
+});
